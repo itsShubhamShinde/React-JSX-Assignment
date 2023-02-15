c9bf75cebce033671770e6d735dc27aedab00380
itsShubhamShinde <itsshubhamks@gmai.com>
2023-02-16 00:06:48 +0530

    Update New Assignment

--- React-JSX-002/index.js
+++ React-JSX-002/index.js
@@ -1,20 +1,17 @@
 
-const about = <div className="div">
-    <h1>About us</h1>
-    <p>At Zappos.com,our purpose is simple: to live and deliver WOW </p>
-    <div>
-    <p>Twenty years ago, we began as a small online retailer that only sold shoes. Today, we still sell shoes - as well as </p>
-    <p>clothing, handbags, accessories, and more. That more is providing the very best customer service, customer</p>
-    <p>experience, and company culture. We aim to inspire the world by showing it's possible to simultaneously deliver</p>
-    <p>happiness to customers, employees, vendors, shareholders, and the community in a long-term, sustainable way.</p>
-    </div>
- 
-    <div>
-    <p>We hope that in the future people won't even realize we started selling shoes online. Instead, they'll know Zappos as a </p>
-    <p>service company that just happens to sell________.</p>
+const airbnb = <div className="borderBox">
+
+    <h1>The Evolution of </h1>
+    <h1>Airbnb's Frontend</h1>
+    <div className="logocont">
+        <p>@spikebrehum</p>
+        <div class="logo">
+            <i class="fa-brands fa-airbnb"></i>
+            <p>airbnb</p>
+        </div>
     </div>
 </div>
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(about)
+root.render(airbnb)
