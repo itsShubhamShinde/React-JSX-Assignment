ce441beb28d675dd80883c0d458819b7f651ead2
itsShubhamShinde <itsshubhamks@gmai.com>
2023-02-15 23:35:42 +0530

    first commit

---
+++ React-JSX-002/index.js
@@ -0,0 +1,20 @@
+
+const about = <div className="div">
+    <h1>About us</h1>
+    <p>At Zappos.com,our purpose is simple: to live and deliver WOW </p>
+    <div>
+    <p>Twenty years ago, we began as a small online retailer that only sold shoes. Today, we still sell shoes - as well as </p>
+    <p>clothing, handbags, accessories, and more. That more is providing the very best customer service, customer</p>
+    <p>experience, and company culture. We aim to inspire the world by showing it's possible to simultaneously deliver</p>
+    <p>happiness to customers, employees, vendors, shareholders, and the community in a long-term, sustainable way.</p>
+    </div>
+ 
+    <div>
+    <p>We hope that in the future people won't even realize we started selling shoes online. Instead, they'll know Zappos as a </p>
+    <p>service company that just happens to sell________.</p>
+    </div>
+</div>
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(about)
